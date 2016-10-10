44f128edf6d086a399c11407e80deced1aefb697
Fuad Balashov <Fuad.Balashov@appian.com>
2016-10-10 16:23:20 -0400

    Initializing App

---
+++ app/src/main/java/fbalashov/imagealigator/MemeClient.java
@@ -0,0 +1,25 @@
+package fbalashov.imagealigator;
+
+import android.app.Application;
+
+import okhttp3.OkHttpClient;
+
+
+public class MemeClient {
+
+
+    private static final String clientId = "99a8cba633b8ee3";
+    private static final String imgurUrl = "https://api.imgur.com/3/memegen/defaults";
+
+    public MemeClient(OkHttpClient client, MemeHandler handler) {
+
+    }
+
+    public void getMeme(final String imageId) {
+
+    }
+
+    public interface MemeHandler {
+        void returnMeme(String link);
+    }
+}
