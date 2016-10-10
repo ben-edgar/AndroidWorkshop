f95b289f0e69ae092591fd0a7afa75b9295e265a
Fuad Balashov <Fuad.Balashov@appian.com>
2016-10-10 16:47:44 -0400

    Click Handler: Adding a toast

--- app/src/main/java/fbalashov/imagealigator/PickerActivity.java
+++ app/src/main/java/fbalashov/imagealigator/PickerActivity.java
@@ -1,7 +1,10 @@
 package fbalashov.imagealigator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PickerActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class PickerActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_picker);
+    Button button = (Button)findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(PickerActivity.this, "It's a trap!", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
