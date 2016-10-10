ea93b1d258471d61882d5ded4f7519bbf196aa42
Fuad Balashov <Fuad.Balashov@appian.com>
2016-10-10 17:28:33 -0400

    Intents: Passing Data

--- app/src/main/java/fbalashov/imagealigator/PickerActivity.java
+++ app/src/main/java/fbalashov/imagealigator/PickerActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class PickerActivity extends AppCompatActivity {
@@ -17,7 +18,9 @@ public class PickerActivity extends AppCompatActivity {
     button.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        EditText editText = (EditText)findViewById(R.id.img_id_view);
         Intent newIntent = new Intent(PickerActivity.this, DetailsActivity.class);
+        newIntent.putExtra(DetailsActivity.IMAGE_ID_EXTRA, editText.getText());
         startActivity(newIntent);
         Toast.makeText(PickerActivity.this, "It's a trap!", Toast.LENGTH_SHORT).show();
       }
