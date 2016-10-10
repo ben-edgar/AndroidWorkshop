0c9c75e290559ea3012d4f05148e026881f98930
Fuad Balashov <Fuad.Balashov@appian.com>
2016-10-10 17:28:33 -0400

    Intents: Passing Data

--- app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
+++ app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
@@ -2,12 +2,17 @@ package fbalashov.imagealigator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+  public static final String IMAGE_ID_EXTRA = "IMAGE_ID";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_details);
+    String imageId = getIntent().getExtras().get(IMAGE_ID_EXTRA).toString();
+    TextView textView = (TextView)findViewById(R.id.post_id);
+    textView.setText(imageId);
   }
 }
