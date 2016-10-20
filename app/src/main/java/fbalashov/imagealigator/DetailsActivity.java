d52740dce28562a1d6fca8be8bdc0f944382ae5c
Brian Kaplan <brian.kaplan@appian.com>
2016-10-20 18:20:48 -0400

    Showing MEME

--- app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
+++ app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
@@ -2,13 +2,17 @@ package fbalashov.imagealigator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import okhttp3.OkHttpClient;
 
 public class DetailsActivity extends AppCompatActivity implements MemeClient.MemeHandler{
   public static final String IMAGE_ID_EXTRA = "IMAGE_ID";
+
+  private ImageView mImageView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class DetailsActivity extends AppCompatActivity implements MemeClient.Mem
     setContentView(R.layout.activity_details);
     String imageId = getIntent().getExtras().get(IMAGE_ID_EXTRA).toString();
     TextView textView = (TextView)findViewById(R.id.post_id);
+    mImageView = (ImageView)findViewById(R.id.imageView);
     textView.setText(imageId);
 
     MemeClient mMemeClient = new MemeClient(new OkHttpClient(), this);
@@ -27,8 +32,7 @@ public class DetailsActivity extends AppCompatActivity implements MemeClient.Mem
     runOnUiThread(new Runnable() {
       @Override
       public void run() {
-        Toast toast = Toast.makeText(DetailsActivity.this, link, Toast.LENGTH_SHORT);
-        toast.show();
+        Picasso.with(DetailsActivity.this).load(link).into(mImageView);
       }
     });
   }
--- app/src/main/java/fbalashov/imagealigator/MemeClient.java
+++ app/src/main/java/fbalashov/imagealigator/MemeClient.java
@@ -1,5 +1,9 @@
 package fbalashov.imagealigator;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -36,7 +40,15 @@ public class MemeClient {
 
             @Override public void onResponse(Call call, Response response) throws IOException {
                 if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-                handler.returnMeme(String.valueOf(response.code()));
+                try {
+                    JSONObject object = new JSONObject(response.body().string());
+                    JSONArray array = object.getJSONArray("data");
+                    final JSONObject memeData = array.getJSONObject(Integer.parseInt(imageId));
+                    String link = memeData.getString("link");
+                    handler.returnMeme(link);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
