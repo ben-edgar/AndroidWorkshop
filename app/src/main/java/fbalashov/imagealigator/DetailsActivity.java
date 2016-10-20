222bacdad75f8df8ea99aa38319908fb8c3313b6
Brian Kaplan <brian.kaplan@appian.com>
2016-10-20 17:38:28 -0400

    Creating an HttpClient

--- app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
+++ app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
@@ -2,9 +2,15 @@ package fbalashov.imagealigator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetailsActivity extends AppCompatActivity {
+import org.json.JSONException;
+
+import okhttp3.OkHttpClient;
+
+public class DetailsActivity extends AppCompatActivity implements MemeClient.MemeHandler{
   public static final String IMAGE_ID_EXTRA = "IMAGE_ID";
 
   @Override
@@ -14,5 +20,19 @@ public class DetailsActivity extends AppCompatActivity {
     String imageId = getIntent().getExtras().get(IMAGE_ID_EXTRA).toString();
     TextView textView = (TextView)findViewById(R.id.post_id);
     textView.setText(imageId);
+
+    MemeClient mMemeClient = new MemeClient(new OkHttpClient(), this.getApplication(), this);
+    mMemeClient.getMeme(imageId);
+  }
+
+  @Override
+  public void returnMeme(final String link) {
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        Toast toast = Toast.makeText(DetailsActivity.this, link, Toast.LENGTH_SHORT);
+        toast.show();
+      }
+    });
   }
 }
