52fb79291d4d4be0bbc437c52c631782ce8c13e7
Brian Kaplan <brian.kaplan@appian.com>
2016-10-20 17:38:28 -0400

    Creating an HttpClient

--- app/src/main/java/fbalashov/imagealigator/MemeClient.java
+++ app/src/main/java/fbalashov/imagealigator/MemeClient.java
@@ -1,25 +1,55 @@
 package fbalashov.imagealigator;
 
 import android.app.Application;
-import org.json.JSONException;
-import okhttp3.OkHttpClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MemeClient {
 
+    private OkHttpClient client;
+    private Application context;
+    private MemeHandler handler;
+    private JSONArray memes;
 
     private static final String clientId = "99a8cba633b8ee3";
     private static final String imgurUrl = "https://api.imgur.com/3/memegen/defaults";
 
     public MemeClient(OkHttpClient client, Application context, MemeHandler handler) {
-
+        this.client = client;
+        this.context = context;
+        this.handler = handler;
     }
 
-    public void getMeme(final String imageId) throws JSONException {
+    public void getMeme(final String imageId) {
+        Request getRequest = new Request.Builder()
+                .url(imgurUrl)
+                .addHeader("Authorization", "Client-ID " + clientId)
+                .build();
 
+        client.newCall(getRequest).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                handler.returnMeme(String.valueOf(response.code()));
+            }
+        });
     }
 
     public interface MemeHandler {
-        void returnMeme(String link) throws JSONException;
+        void returnMeme(String link);
     }
 }
