d4eae890fe9ba7a95307a5ba803ba62e53181d70
Ben Edgar <ben.edgar@appian.com>
2017-02-22 09:44:30 -0500

    Adding MemeClient to DetailsActivity

--- app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
+++ app/src/main/java/fbalashov/imagealigator/DetailsActivity.java
@@ -1,10 +1,13 @@
 package fbalashov.imagealigator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetailsActivity extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+
+public class DetailsActivity extends AppCompatActivity implements MemeClient.MemeHandler{
   public static final String IMAGE_ID_EXTRA = "IMAGE_ID";
 
   @Override
@@ -14,5 +17,19 @@ public class DetailsActivity extends AppCompatActivity {
     String imageId = getIntent().getExtras().get(IMAGE_ID_EXTRA).toString();
     TextView textView = (TextView)findViewById(R.id.post_id);
     textView.setText(imageId);
+
+    MemeClient mMemeClient = new MemeClient(new OkHttpClient(), this);
+    mMemeClient.getMeme(imageId);
+  }
+
+  @Override
+  public void returnMeme(final String link) {
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        Toast toast = Toast.makeText(DetailsActivity.this, link, Toast.LENGTH_SHORT);
+        toast.show();
+      }
+    });
   }
 }
